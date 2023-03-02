fd5073f0623265cb95d85a6a10381322104d7c3f
Jukk1s <jukkahal@metropolia.fi>
2023-03-02 22:09:46 +0200

    SQL script error fixed.

--- src/main/resources/create-database.sql
+++ src/main/resources/create-database.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE IF NOT EXISTS `midieartrainer`;
 USE `midieartrainer`;
 
+CREATE TABLE IF NOT EXISTS `user` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 CREATE TABLE IF NOT EXISTS `game` (
   `GameID` int(11) NOT NULL AUTO_INCREMENT,
   `PlayedAt` timestamp NOT NULL,
@@ -11,12 +19,3 @@ CREATE TABLE IF NOT EXISTS `game` (
   KEY `UserID` (`UserID`),
   CONSTRAINT `game_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `UserID` int(11) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(255) DEFAULT NULL,
-  `Password` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
